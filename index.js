de8f78208bfbdaf0e454d909b81f1a9e932c432e
KevinBett23 <78456086+KevinBett23@users.noreply.github.com>
2021-02-22 02:06:35 +0300

    This commit will add my functions

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ let dayValue;
 const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama"];
+
+const calculateDay = () => {
+  year = document.getElementById("year").value;
+  const CC = parseInt(year.substring(0, 2));
+  const YY = parseInt(year.substring(2, 4));
+  const MM = parseInt(document.getElementById("month").value);
+  const DD = parseInt(document.getElementById("date").value);
+  const d = Math.round(((CC/4) - 2 * CC - 1) + ((5 * YY/4)) + ((26 * (MM+1)/10)) + DD )%7
+  console.log(d);
+  return d;
+};
