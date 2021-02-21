7eef89975d8f08ffc675a58a3961e32cb8bd78f8
KevinBett23 <78456086+KevinBett23@users.noreply.github.com>
2021-02-22 02:02:25 +0300

    This commit will add my arrays

---
+++ index.js
@@ -0,0 +1,4 @@
+let dayValue;
+const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama"];
